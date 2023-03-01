a974f93515eecfe5684678185c95887c7f0d43e8
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-03-01 14:34:14 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 channel = UPDATE_CHANNEL
 
-ft = f"Due To Overload Only Channel Sub Are Use This Bot Join @Oggylink_bulk_Short_Bot."
+ft = f"Due To Overload Only Channel Sub Are Use This Bot Join @Netflix_hindi_Movies_4K2."
 
 
 # Private Chat
